a5b1113a6208fcf2be0db0755c7673d2347a6093
Travis Reed <tdreed@gmail.com>
2023-03-23 01:57:18 -0700

    Correct progress time

--- src/process.ts
+++ src/process.ts
@@ -3,16 +3,14 @@ import { MultiBar, SingleBar, Presets } from "cli-progress";
 import { keys } from "./layout";
 
 export function processKeysSync(inclusionPattern: RegExp = /.+/): void {
+    const filteredKeys = keys.filter(key => inclusionPattern.test(key.id));
     const bar = new SingleBar({
         clearOnComplete: false,
         hideCursor: true,
     }, Presets.shades_grey);
-    bar.start(keys.length, 0);
-    for (const key of keys) {
+    bar.start(filteredKeys.length, 0);
+    for (const key of filteredKeys) {
         bar.increment();
-        if (!inclusionPattern.test(key.id)) {
-            continue
-        }
         key.writeScadFile();
         key.convertSync();
     }
@@ -20,6 +18,8 @@ export function processKeysSync(inclusionPattern: RegExp = /.+/): void {
 }
 
 export async function processKeys(inclusionPattern: RegExp = /.+/): Promise<void> {
+    const filteredKeys = keys.filter(key => inclusionPattern.test(key.id));
+
     const finishedJobs: (ChildProcess | null)[] = [];
     const errors: Error[] = [];
 
@@ -32,11 +32,8 @@ export async function processKeys(inclusionPattern: RegExp = /.+/): Promise<void
     const stl = bars.create(keys.length, 0);
 
     return new Promise((resolve, reject) => {
-        for (const key of keys) {
+        for (const key of filteredKeys) {
             scad.increment();
-            if (!inclusionPattern.test(key.id)) {
-                continue
-            }
             key.writeScadFile();
             const finish = (job: ChildProcess | null) => {
                 finishedJobs.push(job);
